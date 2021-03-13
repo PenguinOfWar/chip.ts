e304449d595edffc30bec17dbd5063425ced4e5a
Darryl Walker <darryljwalker@gmail.com>
2021-03-13 22:04:35 +0000

    Hours of reading and mostly writing comments!

--- src/libs/chip/drivers/cpu.ts
+++ src/libs/chip/drivers/cpu.ts
@@ -102,24 +102,89 @@ export default class Cpu {
      * https://github.com/alexanderdickson/Chip-8-Emulator/blob/5da6ac6a4753462d02ca7fe8d5a9398308b8d9d0/scripts/chip8.js#L193
      * We're shifting everything over bitwise 8 to the left
      * And then doing a bitwise or on each pair of bits. a OR b yields 1 if either a or b is 1.
+     *
+     * UPDATE
+     * Bless this dude, who seems to be the reference for the above JS dude/dudette
+     * https://github.com/eshyong/Chip-8-Emulator/blob/master/chip8.c
+     *
+     * Opcodes are two bytes long!
+     * We're creating a hexidecimal here, I guess? Out of the two byte opcode
+     *
+     * here we take the first complete bite
+     * in pongs case that's 6A hex / 106 decimalÎ
+     * we also take the next byte which is conveniently 02 hex / 2 decimal
+     *
+     * fuck me
+     * i guess this is a clever way of storing integers greater than 8 bits
+     * which is cool as shit
+     *
+     * we take decimal 106 and we shift it left 8 places giving us decimal 27136
+     *
+     * then we take output of that left shift and give it to a bitwise OR - the single pipe dude
+     * this guy adds them together
+     * problem: 27136 | 2
+     * result: 27138 decimal / 6a02 hex
+     *
+     * i guess we just got two bytes of storage out of one byte of data
+     * how very... bitwise
+     *
      */
+
     const opcode =
       (this.memory[this.counter] << 8) | this.memory[this.counter + 1];
 
     /**
      * here a bitwise AND (&) compares two numbers and casts them to numbers if they arent numbers
      * my head hurts
+     *
+     * Read here:
+     * https://en.wikipedia.org/wiki/Bitwise_operation#AND
+     *
+     * Okay.
+     * There's a lot going on here.
+     * X is a two parter, first we take our opcode 27136 and give it 0x0f00 hex / 3840 decimal
+     * this yields a00 hex / 2560 decimal
+     *
+     * we then shift the result over 8 places right yielding a hex / 10 decimal
+     *
+     * do similar for y taking 27136 and giving it 0x0000 hex / 0 decimal and go god i keep flopping back and forth
+     * it's all hex i swear just different hex and i will tidy it up just look at the decimals
+     *
+     * shift it over 4 and it's still 0 / 0x0000
+     *
      */
 
     const x = (opcode & 0x0f00) >> 8;
     const y = (opcode & 0x00f0) >> 4;
 
-    console.log(this.counter, opcode, x, y, opcode & 0xf000);
+    if (opcode === 27138) {
+      console.log(27138, opcode & 0x0f00, x);
+      console.log(27138, opcode & 0x00f0, y);
+    }
 
     /**
-     * increment our counter to move to the next instruction
+     * all of our instructions are two bytes long
+     * increment our counter+2 to move to the next instruction by default
      */
 
     this.counter += 2;
+
+    /**
+     * then start checking opcodes
+     *
+     * our first check is the result of opcode AND 0xf000
+     * continuing with the above we have 27138 & 61440 = 24576 decimal / 0x6000 hex
+     *
+     * this corresponds to opcode 6XNN!
+     */
+
+    switch (opcode & 0xf000) {
+      case 0x6000: {
+        console.log('Setting VX to NN');
+        break;
+      }
+      default: {
+      }
+    }
   }
 }
